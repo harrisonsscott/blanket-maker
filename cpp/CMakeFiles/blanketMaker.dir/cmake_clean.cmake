3662d098c2ee7adcb817afc87d38ab2a8aed4034
harrisonsscott <harrisonsterlingscott@gmail.com>
2024-01-26 15:47:19 -0800

    Added OpenCV for C++

---
+++ cpp/CMakeFiles/blanketMaker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blanketMaker.dir/main.cpp.o"
+  "CMakeFiles/blanketMaker.dir/main.cpp.o.d"
+  "blanketMaker"
+  "blanketMaker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blanketMaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+class Color {
+	public:
+		int r;
+		int g;
+		int b;
+};
+
+float getSimilarity(Color color1, Color color2){
+	return
+		0.3 * std::pow(color1.r - color2.r, 2) +
+		0.59 * std::pow(color1.g - color2.g, 2) +
+		0.11 * std::pow(color1.b - color2.b, 2);
+}
+
+int* hexToRGB(std::string hex){
+	hex.erase(std::remove(hex.begin(), hex.end(), '#'), hex.end());
+	return new int[2];
+}
+
+int main(int argc, char *argv[]){
+	if (argc < 4){
+		std::cout << "main.py [image file] [output size x] [output size y]" << std::endl;
+		return -1;
+	}
+
+	int width = std::stoi(argv[2]);
+	int height = std::stoi(argv[3]);
+
+	Mat im = imread(argv[1]);
+
+	resize(im, im, Size(width, height));
+
+	if (im.empty()){
+		std::cout << "Can't find image!" << std::endl;
+		return -1;
+	}
+
+	for (int x = 0; x < im.rows; x++){
+		for (int y = 0; y < im.cols; y++ ){
+			Vec3b & color = im.at<Vec3b>(x,y);
+			std::cout << color << std::endl;
+
+			im.at<Vec3b>(Point(y,x)) = color;
+		}
+	}
+
+	imwrite("bar.png", im);
+
+	return 0;
+}
